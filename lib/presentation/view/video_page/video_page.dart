f8246b26c6bc622adef1ef46722dfde72853b308
choiJunHo <wnsghqwe@gmail.com>
2023-03-06 23:39:05 +0900

    perf: video_page.dart 완성

--- lib/presentation/view/video_page/video_page.dart
+++ lib/presentation/view/video_page/video_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:test_app/presentation/widget/loading_indicator.dart';
+import 'package:video_player/video_player.dart';
 
 import '../../view_model/video_view_model.dart';
 
@@ -12,18 +14,81 @@ class VideoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<VideoViewModel>(
-        init: VideoViewModel(),
+        init: VideoViewModel(url: url),
         builder: (vm) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text("비디오 페이지"),
-              centerTitle: true,
-            ),
-            body: Container(
-              child: Column(
-                children: [Text("video")],
-              ),
-            ),
+          return Obx(
+            () => Scaffold(
+                appBar: vm.isRotate.isFalse
+                    ? AppBar(
+                        title: const Text("비디오 페이지"),
+                        centerTitle: true,
+                      )
+                    : null,
+                backgroundColor: Colors.black,
+                body: vm.isLoaded.isTrue
+                    ? GestureDetector(
+                        onTap: () => vm.optionOpened.toggle(),
+                        child: Center(
+                          child: AspectRatio(
+                            aspectRatio: vm.aspectRatio,
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                VideoPlayer(vm.controller),
+                                Positioned(
+                                  child: AnimatedOpacity(
+                                    opacity: vm.optionOpened.isTrue ? 1 : 0,
+                                    duration:
+                                        const Duration(milliseconds: 300),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        IconButton(
+                                            onPressed: () => vm.back(),
+                                            icon: Icon(
+                                              Icons.fast_rewind_outlined,
+                                              size: 48,
+                                              color: Colors.white,
+                                            )),
+                                        IconButton(
+                                            onPressed: () => vm.playAndStop(),
+                                            icon: Icon(
+                                              vm.isPlaying.isTrue
+                                                  ? Icons.pause
+                                                  : Icons.play_circle,
+                                              size: 48,
+                                              color: Colors.white,
+                                            )),
+                                        IconButton(
+                                            onPressed: () => vm.forward(),
+                                            icon: Icon(
+                                              Icons.fast_forward_outlined,
+                                              size: 48,
+                                              color: Colors.white,
+                                            )),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                    right: 0,
+                                    bottom: 0,
+                                    child: IconButton(
+                                        onPressed: () => vm.fullScreen(),
+                                        icon: Icon(
+                                          vm.isRotate.isTrue
+                                              ? Icons.fullscreen_exit
+                                              : Icons.fullscreen,
+                                          size: 24,
+                                          color: Colors.white,
+                                        )))
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    : const LoadingIndicator()),
           );
         });
   }
--- lib/presentation/view_model/video_view_model.dart
+++ lib/presentation/view_model/video_view_model.dart
@@ -1,5 +1,70 @@
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoViewModel extends GetxController {
+  VideoViewModel({required this.url});
 
+  RxBool isRotate = false.obs;
+  RxBool isLoaded = false.obs;
+  RxBool isPlaying = false.obs;
+  RxBool optionOpened = false.obs;
+
+  final String url;
+
+  late final double aspectRatio;
+  late final VideoPlayerController controller;
+
+  @override
+  void onInit() async {
+    controller = VideoPlayerController.network(url);
+    await controller.initialize();
+    controller
+      ..setVolume(1)
+      ..play();
+    aspectRatio = controller.value.aspectRatio;
+    isLoaded.value = true;
+    isPlaying.value = true;
+    super.onInit();
+  }
+
+  playAndStop() {
+    if (isPlaying.isTrue) {
+      controller.pause();
+    } else {
+      controller.play();
+    }
+    isPlaying.toggle();
+  }
+
+  void fullScreen() {
+    isRotate.toggle();
+    if (isRotate.isTrue) {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeRight,
+        DeviceOrientation.landscapeLeft,
+      ]);
+    } else {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
+    }
+  }
+
+  void back() async {
+    await controller
+        .seekTo(controller.value.position - const Duration(seconds: 15));
+  }
+
+  void forward() async {
+    await controller
+        .seekTo(controller.value.position + const Duration(seconds: 15));
+  }
+
+  @override
+  void onClose() {
+    controller.dispose();
+    super.onClose();
+  }
 }
